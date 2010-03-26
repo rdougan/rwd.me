f9bb7a4aacaa13d1ec3d1938a901decb1590212e
Robert Dougan <rdougan@me.com>
2010-03-26 02:25:22 +0000

    ...

--- config/environment.rb
+++ config/environment.rb
@@ -18,7 +18,8 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
-  # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem "flickr", :version => "1.0.2"
+  config.gem "xml-simple", :version => "1.0.12"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
