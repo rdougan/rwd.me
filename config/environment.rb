b01c5ecbd4ed3a7b86835e3e4aa6a30c004736bf
Robert Dougan <rdougan@me.com>
2010-05-14 16:11:40 +0100

    ...

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'twitter', :version => '0.9.1'
+  # config.gem 'twitter', :version => '0.9.1'
   
   config.time_zone = 'UTC'
 end
